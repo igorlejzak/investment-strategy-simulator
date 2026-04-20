bd686cbfae10bff26a4e5d3a6ba5b0a58a69db70
igorlejzak <pinga3009@gmail.com>
2026-04-21 00:24:53 +0200

    remove old files

--- game/game_engine.py
+++
@@ -1,3 +0,0 @@
-class FinanceEngine:
-    def calculate_new_capital(self, capital, return_rate):
-        return capital * (1 + return_rate)
--- simulation/strategy_generator.py
+++
@@ -1,5 +0,0 @@
-import itertools
-
-class StrategyGenerator:
-    def get_all_paths(self, assets, num_periods):
-        return list(itertools.product(assets, repeat=num_periods))
